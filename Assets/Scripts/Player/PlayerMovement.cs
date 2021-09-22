a2c82b67838f3f7368583a8ade9e5eb1b137275c
Hunter Hughes <hsh3n3@umsystem.edu>
2021-09-22 18:24:41 -0500

    Added Crouching and upward collision detection.
    Updated to add upward collision checks to fix jumping under something, and stepOffset changes to make jumping up against vertical objects smoother.
    
    Also added crouch ability, plus some more checks to keep air speed consistent between standing/crouching. Cannot jump while crouching. - need to fix standing up under objects above you while crouching.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,8 @@
-//Originally created by Hunter Hughes on 9-21-2021
+/*Originally created by Hunter Hughes on 9-21-2021
+9-22-2021 Hunter - Updated to add upward collision checks to fix jumping under something, and stepOffset changes to make jumping up against vertical objects smoother.
+                   Also added crouch ability, plus some more checks to keep air speed consistent between standing/crouching. Cannot jump while crouching. - need to fix standing up under objects above you while crouching.
+*/
+
 
 using System.Collections;
 using System.Collections.Generic;
@@ -9,26 +13,95 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller; //Character object in scene that we can make move
 
     public float speed = 12f; //movement speed
+    public float walkSpeed = 12f;
+    public float crouchSpeed = 6f;
     public float gravity = -19.62f; //Earth's gravity * 2. Doubled because regular gravity feels a bit too "floaty" in video game world.
     public float jumpHeight = 3f;//jump height
+    public Vector3 playerHeight = new Vector3(1,1,1);
+    public Vector3 crouchHeight = new Vector3(1, 0.65f, 1);
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public Transform groundCheck; //To check if you are on the ground.
+    public float groundDistance = 0.4f; 
     public LayerMask groundMask; //To set a layer in unity to be 'ground' objects for collision checking.
+
+    public float originalStepOffset = 0.7f;
+    public float originaljumpHeight = 3f;
+
 
     Vector3 velocity;
     bool isGrounded; //Checks if player is on ground or not
+    bool isCrouching;
+    bool isAirborn;
 
     // Update is called once per frame
     void Update()
     {
         //creates tiny sphere that will check collision with anything under the player. If so, will set isGrounded = true.
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        //This condition is to check for things above the character. If you hit a ceiling, make the character fall back down.
+        if ((controller.collisionFlags & CollisionFlags.Above) != 0)
         {
-            velocity.y = -2f; //forces our player all the way to the ground.
+            if (velocity.y > 0)
+            {
+                velocity.y = -velocity.y;
+            }
         }
+        
+        
+        if (isGrounded && velocity.y < 0)
+         {
+            velocity.y = -2f; //forces our player all the way to the ground.
+            controller.stepOffset = originalStepOffset; // make it so you can climb small objects, such as stairs when walking.
+            isAirborn = false;
+         }
+
+
+        //Check to see if we are in the air
+        if(velocity.y > 0)
+        {
+            isAirborn = true;
+        }
+
+        else
+        {
+            controller.stepOffset = 0; //Makes jumping up against a vertical object smooth by removing step offset.
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            isCrouching = true;
+        }
+        else
+        {
+            isCrouching = false;
+        }
+
+
+        //if crouching, set crouching height and remove jump
+        if (isCrouching)
+        {
+            //controller.height = crouchHeight;
+            jumpHeight = 0f;
+            speed = crouchSpeed;
+            controller.gameObject.transform.localScale = crouchHeight;
+        }
+        //revert to non-crouch settings
+        else
+        {
+            //controller.height = playerHeight;
+            jumpHeight = originaljumpHeight;
+            speed = walkSpeed;
+            controller.gameObject.transform.localScale = playerHeight;
+        }
+
+        //keep speed consistent in the air
+        if (isAirborn)
+        {
+            speed = walkSpeed;
+        }
+
+
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
