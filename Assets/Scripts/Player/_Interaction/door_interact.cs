e75da3e42bafb2cb9a16ee9e4a8f937c7452e1e9
Hunter Hughes <hsh3n3@umsystem.edu>
2021-10-19 13:37:07 -0500

    n.a

--- Assets/Scripts/Player/_Interaction/door_interact.cs
+++ Assets/Scripts/Player/_Interaction/door_interact.cs
@@ -6,26 +6,27 @@ using UnityEngine.UI;
 
 public class door_interact : MonoBehaviour
 {
-    private bool isUnlocked;
-    public InventoryObject inventory;
+    private bool isUnlocked = false;
+    private Transform door;
+    private bool doOnce = false;
 
     public void Start()
     {
+        door = this.transform;
     }
 
     public void Update()
     {
-        if (isUnlocked)
+        if (isUnlocked && doOnce)
         {
-        
+            door.localPosition = new Vector3(-2, 0, -2);
+            door.localRotation = new Quaternion(0, -90, 0, 0);
+            doOnce = false;
         }
     }
     public void Interact()
     {
         isUnlocked = true;
-
-
-
-
+        doOnce = true;
     }
 }
