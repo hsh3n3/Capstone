8e2d9fbb60a1437a519e516a0fef9695b62e0fd1
Will Slama <wcslam99@gmail.com>
2021-11-02 10:06:05 -0500

    MoveTexture.cs

---
+++ Assets/Scripts/MoveTexture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTexture : MonoBehaviour {
+
+	public Material mat;
+
+    [System.Serializable]
+    public class property
+    {
+        public string PropertyName;
+        public Vector2 speed;
+    }
+    public property[] properties;
+
+    void FixedUpdate () {
+        foreach (property prop in properties)
+        {
+            string p = prop.PropertyName;
+
+            mat.SetTextureOffset(p, new Vector2(
+                ((mat.GetTextureOffset(p).x + (prop.speed.x * Time.deltaTime)) % mat.GetTextureScale(p).x),
+                (mat.GetTextureOffset(p).y + (prop.speed.y * Time.deltaTime)) % mat.GetTextureScale(p).y));
+        }
+	}
+}
